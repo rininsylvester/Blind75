1dd61143c004bf74817187a3ead756c79f9098d2
rininsylvester <l.rininsylvester@gmail.com>
2025-05-07 14:52:31 +0530

    Create Reverse Bits.py

---
+++ Reverse Bits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            bits = (n>>i) & 1
+            res = res | (bits<<(31-i))
+        return res
